b98f4d48446b17b2890b6b004207fae6883ee93b
icerealm <icerealm@hotmail.com>
2017-10-03 23:48:57 +0700

    add logic for image checking

--- initial_scrapy.sh
+++ initial_scrapy.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-if [ -d "$1" ]; then
-    echo "executing docker run"
-    docker run --name my-scrapy -v $1:/opt/dev -t -d scrapy
+docker=`which docker`
+image_name=`$docker images | grep scrapy | awk '{print $1}'`
+
+if [ ${#image_name} -gt 0 ]; then
+    if [ -d "$1" ]; then
+        echo "executing docker run"
+        docker run --name my-scrapy -v $1:/opt/dev -t -d scrapy
+    else
+        echo "please specify your local directory. \nEg) ./initial_scrapy.sh <your_local_dir>"
+    fi
 else
-    echo "$1 does not exist"
+    echo "there is no scrapy image in your system, please run 'docker build -t scrapy .'"
 fi
